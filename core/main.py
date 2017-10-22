9c211bf9193846178a3498ab696787e06e90490c
teessider <andrew@andybellart.co.uk>
2017-10-22 05:42:39 +0200

    So a fair amount has changed! The formatting of the introduction has been done. The choice numbers are now in a list (good if this changes and expands in the future). This has been implemented in the rest of the program. Main menu has been done "visually" - needs full functionality. Started on the Swizzling existing textures menu. Also came up with a couple of other possible names.

--- core/main.py
+++ core/main.py
@@ -1,33 +1,80 @@
+import sys
 from PIL import Image
 
 
 class Program(object):
-    intro_text = "////////////////////////////\nWelcome to the Simple Texture Packer.\n" \
-              "Created by Andrew \'teessider\' Bell.\n//////////////////////\n" \
-              "This tool is for packing textures without the need to go into more advanced programs.\n" \
-              "The choices are as follows:\n" \
-              "1) Create new packed texture from existing ones\n" \
-              "2) Repack a pre-packed texture\n"
+    __fluff = "*" * 50  # Purely cosmetic purpose ;)
+    __choices = (1, 2, 3)
+
+    __intro_text = "{0}\n" \
+                   "Welcome to the Simple Texture Packer 0.1\nCreated By\nAndrew \'teessider\' Bell.\n" \
+                   "{0}\n" \
+                   "This command line tool is for packing textures without the need to go into more advanced programs.\n".format(
+        __fluff)
+    # For string formatting which includes __choices list, I use automatic one as the choices could expand in the future
+    __main_choice_text = "Select:\n{}) Create new packed texture from existing ones\n{}) Repack (Swizzle) a pre-packed texture \n{}) Exit Program\n\n".format(
+        *__choices)
 
     def __init__(self):
-        print(self.intro_text)
+        self.choice = 0
+        print(self.__intro_text)
+        self.main_menu()
+
+    def main_menu(self):
+        """Displays the main menu of the program which are the user's main choices. Includes exiting the program.
+        """
         while True:
             try:
-                self.choice = int(input("What would you like to do?\n"))
-                if self.choice == 1:
-                    print("Cool choice!")
-                if self.choice == 2:
-                    print("Most excellent!")
+                self.choice = int(self.user_input(self.__main_choice_text))
+                if self.choice == self.__choices[0] or self.__choices[1]:
+                    break
+                elif self.choice == self.__choices[-1]:
+                    sys.exit(0)
                 else:
                     self.error_response("Invalid number")
                     continue
             except ValueError:
                 self.error_response("It must be a number")
                 continue
+        if self.choice == self.__choices[0]:
+            print("TO DO")  # Nothing for now ;)
+            # 1) Create new texture (from existing ones)
+            #   1a) Size - preset or user defined (eg. non-square)
+        elif self.choice == self.__choices[1]:
+            self.swizzle_menu()
 
     @staticmethod
-    def error_response(response):
-        print("{0}! Please pick 1 or 2".format(response))
+    def user_input(text):
+        return input("{0}What would you like to do?\n".format(text))
+
+    def error_response(self, response):
+        print("{}! Please pick {}, {} or {}\n".format(response, *self.__choices))
+
+    def swizzle_menu(self):
+        """The menu which the user can repack(swizzle) an existing packed texture.\n
+                The user can optionally add an alpha channel too."""
+        swizzle_menu_text = "Select:\n{}) Swizzle (Swap channels)\n{}) Add Alpha Channel\n{}) Back to Main Menu\n".format(
+            *self.__choices)
+        # 2) Repack existing texture
+        #   2a) Swap channels
+        #   2b) Add Alpha Channel
+        while True:
+            try:
+                self.choice = int(self.user_input(swizzle_menu_text))
+                if self.choice == self.__choices[0] or self.__choices[1] or self.__choices[-1]:
+                    break
+                else:
+                    self.error_response("Invalid number")
+                    continue
+            except ValueError:
+                self.error_response("It must be a number")
+                continue
+        if self.choice == self.__choices[0]:
+            pass
+        elif self.choice == self.__choices[1]:
+            pass
+        elif self.choice == self.__choices[-1]:
+            self.main_menu()
 
 # TODO: User input for selecting images
 # image1 = "E:\Projects\\test\\test1.tga"
@@ -45,14 +92,7 @@ class Program(object):
 # except IOError:
 #     print("Can't open an image, Invalid file path or it doesn't exist!")
 
-# Process (cmd options):
-# 1) Create new texture (from existing ones)
-#   1a) Size - preset or user defined (eg. non-square)
-# 2) Repack existing texture
-#   2a) Swap channels
-#   2b) Add Alpha Channel
-
-# Name - Mr Swizzler?
+# Names - TextureBox, SwizzleBox, Texture Swizzler, Channel Packer
 
 if __name__ == '__main__':
     Program()
